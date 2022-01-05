adeaabdc2c74c10e27f7b98b7254d7762ae9d9f5
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-01-05 14:36:39 -0600

    Ajustes de sintaxis

--- s-02-ilap-ligas.sql
+++ s-02-ilap-ligas.sql
@@ -14,9 +14,7 @@ connect ilap_bdd/ilap_bdd@crvbdd_s1
 create database link crvbdd_s2.fi.unam using 'CRVBDD_S2';
 --PDB remotas
 create database link arcbd_s1.fi.unam using 'CKVBDD_S1';
-Create database link arcbd_s2.fi.unam using 'CKVBDD_S2';
---completar
-prompt Listo!
+create database link arcbd_s2.fi.unam using 'CKVBDD_S2';
 
 prompt ============================
 prompt CREANDO LIGAS EN: crvbdd_s2
@@ -26,9 +24,7 @@ connect ilap_bdd/ilap_bdd@crvbdd_s2
 create database link crvbdd_s1.fi.unam using 'CRVBDD_S1';
 --PDB remotas
 create database link arcbd_s1.fi.unam using 'CKVBDD_S1';
-Create database link arcbd_s2.fi.unam using 'CKVBDD_S2';
---completar
-prompt Listo!
+create database link arcbd_s2.fi.unam using 'CKVBDD_S2';
 
 prompt ============================
 prompt CREANDO LIGAS EN: ckvbdd_s1
@@ -38,9 +34,7 @@ connect ilap_bdd/ilap_bdd@ckvbdd_s1
 create database link ckvbdd_s2.fi.unam using 'CKVBDD_S2';
 --PDB remotas
 create database link crvbdd_s1.fi.unam using 'CRVBDD_S1';
-Create database link crvbdd_s2.fi.unam using 'CRVBDD_S2';
---completar
-prompt Listo!
+create database link crvbdd_s2.fi.unam using 'CRVBDD_S2';
 
 prompt ============================
 prompt CREANDO LIGAS EN: ckvbdd_s2
@@ -52,4 +46,4 @@ create database link ckvbdd_s1.fi.unam using 'CKVBDD_S1';
 create database link crvbdd_s1.fi.unam using 'CRVBDD_S1';
 Create database link crvbdd_s2.fi.unam using 'CRVBDD_S2';
 --completar
-prompt Listo!
+prompt LISTO!
