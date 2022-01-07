1f29b5160b877fcaa0e43aa44133c3a04e9614aa
Caro Kennedy <carosiim.sdco@gmail.com>
2022-01-07 14:01:38 -0600

    Update sinonimos 4

--- src/s-04-ilap-crv-s1-sinonimos.sql
+++ src/s-04-ilap-crv-s1-sinonimos.sql
@@ -5,55 +5,55 @@
 
 connect ilap_bdd/ilap@crvbdd_s1
 
-create sucursal_f1 for sucursal_f1_ckv_s1@ckvbdd_s1;
-create sucursal_f2 for sucursal_f2_ckv_s2@ckvbdd_s2;
-create sucursal_f3 for sucursal_f3_crv_s1;
-create sucursal_f4 for sucursal_f4_crv_s2@crvbdd_s2;
+create or replace synonym sucursal_f1 for sucursal_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym sucursal_f2 for sucursal_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym sucursal_f3 for sucursal_f3_crv_s1;
+create or replace synonym sucursal_f4 for sucursal_f4_crv_s2@crvbdd_s2;
 
-create sucursal_taller_f1 for sucursal_taller_f1_ckv_s1@ckvbdd_s1;
-create sucursal_taller_f2 for sucursal_taller_f2_ckv_s2@ckvbdd_s2;
-create sucursal_taller_f3 for sucursal_taller_f3_crv_s1;
-create sucursal_taller_f4 for sucursal_taller_f4_crv_s2@crvbdd_s2;
+create or replace synonym sucursal_taller_f1 for sucursal_taller_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym sucursal_taller_f2 for sucursal_taller_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym sucursal_taller_f3 for sucursal_taller_f3_crv_s1;
+create or replace synonym sucursal_taller_f4 for sucursal_taller_f4_crv_s2@crvbdd_s2;
 
-create sucursal_venta_f1 for sucursal_venta_f1_ckv_s1@ckvbdd_s1;
-create sucursal_venta_f2 for sucursal_venta_f2_ckv_s2@ckvbdd_s2;
-create sucursal_venta_f3 for sucursal_venta_f3_crv_s1;
-create sucursal_venta_f4 for sucursal_venta_f4_crv_s2@crvbdd_s2;
+create or replace synonym sucursal_venta_f1 for sucursal_venta_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym sucursal_venta_f2 for sucursal_venta_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym sucursal_venta_f3 for sucursal_venta_f3_crv_s1;
+create or replace synonym sucursal_venta_f4 for sucursal_venta_f4_crv_s2@crvbdd_s2;
 
-create servicio_laptop_f1 for servicio_laptop_f1_ckv_s1@ckvbdd_s1;
-create servicio_laptop_f2 for servicio_laptop_f2_ckv_s2@ckvbdd_s2;
-create servicio_laptop_f3 for servicio_laptop_f3_crv_s1;
-create servicio_laptop_f4 for servicio_laptop_f4_crv_s2@crvbdd_s2;
+create or replace synonym servicio_laptop_f1 for servicio_laptop_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym servicio_laptop_f2 for servicio_laptop_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym servicio_laptop_f3 for servicio_laptop_f3_crv_s1;
+create or replace synonym servicio_laptop_f4 for servicio_laptop_f4_crv_s2@crvbdd_s2;
 
-create laptop_f1 for laptop_f1_ckv_s1@ckvbdd_s1;
-create laptop_f2 for laptop_f2_ckv_s2@ckvbdd_s2;
-create laptop_f3 for laptop_f3_crv_s1;
-create laptop_f4 for laptop_f4_crv_s2@crvbdd_s2;
-create laptop_f5 for laptop_f5_crv_s2@crvbdd_s2;
+create or replace synonym laptop_f1 for laptop_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym laptop_f2 for laptop_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym laptop_f3 for laptop_f3_crv_s1;
+create or replace synonym laptop_f4 for laptop_f4_crv_s2@crvbdd_s2;
+create or replace synonym laptop_f5 for laptop_f5_crv_s2@crvbdd_s2;
 
-create laptop_inventario_f1 for laptop_inventario_f1_ckv_s1@ckvbdd_s1;
-create laptop_inventario_f2 for laptop_inventario_f2_ckv_s2@ckvbdd_s2;
-create laptop_inventario_f3 for laptop_inventario_f3_crv_s1;
+create or replace synonym laptop_inventario_f1 for laptop_inventario_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym laptop_inventario_f2 for laptop_inventario_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym laptop_inventario_f3 for laptop_inventario_f3_crv_s1;
 
-create historico_status_f1 for historico_status_f1_ckv_s1@ckvbdd_s1;
-create historico_status_f2 for historico_status_f2_ckv_s2@ckvbdd_s2;
+create or replace synonym historico_status_f1 for historico_status_f1_ckv_s1@ckvbdd_s1;
+create or replace synonym historico_status_f2 for historico_status_f2_ckv_s2@ckvbdd_s2;
 
-create tipo_monitor_r1 for tipo_monitor_r_ckv_s1@ckvbdd_s1;
-create tipo_monitor_r2 for tipo_monitor_r_ckv_s2@ckvbdd_s2;
-create tipo_monitor_r3 for tipo_monitor_r_crv_s1;
-create tipo_monitor_r4 for tipo_monitor_r_crv_s2@crvbdd_s2;
+create or replace synonym tipo_monitor_r1 for tipo_monitor_r_ckv_s1@ckvbdd_s1;
+create or replace synonym tipo_monitor_r2 for tipo_monitor_r_ckv_s2@ckvbdd_s2;
+create or replace synonym tipo_monitor_r3 for tipo_monitor_r_crv_s1;
+create or replace synonym tipo_monitor_r4 for tipo_monitor_r_crv_s2@crvbdd_s2;
 
-create tipo_almacenamiento_r1 for tipo_almacenamiento_r_ckv_s1@ckvbdd_s1;
-create tipo_almacenamiento_r2 for tipo_almacenamiento_r_ckv_s2@ckvbdd_s2;
-create tipo_almacenamiento_r3 for tipo_almacenamiento_r_crv_s1;
-create tipo_almacenamiento_r4 for tipo_almacenamiento_r_crv_s2@crvbdd_s2;
+create or replace synonym tipo_almacenamiento_r1 for tipo_almacenamiento_r_ckv_s1@ckvbdd_s1;
+create or replace synonym tipo_almacenamiento_r2 for tipo_almacenamiento_r_ckv_s2@ckvbdd_s2;
+create or replace synonym tipo_almacenamiento_r3 for tipo_almacenamiento_r_crv_s1;
+create or replace synonym tipo_almacenamiento_r4 for tipo_almacenamiento_r_crv_s2@crvbdd_s2;
 
-create tipo_procesador_r1 for tipo_procesador_r_ckv_s1@ckvbdd_s1;
-create tipo_procesador_r2 for tipo_procesador_r_ckv_s2@ckvbdd_s2;
-create tipo_procesador_r3 for tipo_procesador_r_crv_s1;
-create tipo_procesador_r4 for tipo_procesador_r_crv_s2@crvbdd_s2;
+create or replace synonym tipo_procesador_r1 for tipo_procesador_r_ckv_s1@ckvbdd_s1;
+create or replace synonym tipo_procesador_r2 for tipo_procesador_r_ckv_s2@ckvbdd_s2;
+create or replace synonym tipo_procesador_r3 for tipo_procesador_r_crv_s1;
+create or replace synonym tipo_procesador_r4 for tipo_procesador_r_crv_s2@crvbdd_s2;
 
-create tipo_tarjeta_video_r1 for tipo_tarjeta_video_r_ckv_s1@ckvbdd_s1;
-create tipo_tarjeta_video_r2 for tipo_tarjeta_video_r_ckv_s2@ckvbdd_s2;
-create tipo_tarjeta_video_r3 for tipo_tarjeta_video_r_crv_s1;
-create tipo_tarjeta_video_r4 for tipo_tarjeta_video_r_crv_s2@crvbdd_s2;
+create or replace synonym tipo_tarjeta_video_r1 for tipo_tarjeta_video_r_ckv_s1@ckvbdd_s1;
+create or replace synonym tipo_tarjeta_video_r2 for tipo_tarjeta_video_r_ckv_s2@ckvbdd_s2;
+create or replace synonym tipo_tarjeta_video_r3 for tipo_tarjeta_video_r_crv_s1;
+create or replace synonym tipo_tarjeta_video_r4 for tipo_tarjeta_video_r_crv_s2@crvbdd_s2;
