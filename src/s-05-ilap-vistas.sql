3fbf9cd436a8c210f3c6140539d3d454c4687aef
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-01-07 14:31:30 -0600

    Script para vistas

---
+++ src/s-05-ilap-vistas.sql
@@ -0,0 +1,68 @@
+-- @Autor: Carolina Kennedy Villa
+--         C. Mauricio Ramos Villaseñor
+-- @Fecha creación: 07/01/2021
+-- @Descripción: Creación de vistas que no requieren manejo de BLOBs.
+
+--
+-- SUCURSAL
+--
+create or replace view sucursal as
+select sucursal_id,clave,es_taller,es_venta,nombre,latitud,longitud,url from sucursal_f1
+union all
+select sucursal_id,clave,es_taller,es_venta,nombre,latitud,longitud,url from sucursal_f2
+union all
+select sucursal_id,clave,es_taller,es_venta,nombre,latitud,longitud,url from sucursal_f3
+union all
+select sucursal_id,clave,es_taller,es_venta,nombre,latitud,longitud,url from sucursal_f4;
+
+--
+-- SUCURSAL_VENTA
+--
+create or replace view sucursal_venta as
+select sucursal_id,hora_apertura,hora_cierre from sucursal_venta_f1
+union all
+select sucursal_id,hora_apertura,hora_cierre from sucursal_venta_f2
+union all
+select sucursal_id,hora_apertura,hora_cierre from sucursal_venta_f3
+union all
+select sucursal_id,hora_apertura,hora_cierre from sucursal_venta_f4;
+
+--
+-- SUCURSAL_TALLER
+--
+create or replace view sucursal_taller as
+select sucursal_id,dia_descanso,telefono_atencion from sucursal_taller_f1
+union all
+select sucursal_id,dia_descanso,telefono_atencion from sucursal_taller_f2
+union all
+select sucursal_id,dia_descanso,telefono_atencion from sucursal_taller_f3
+union all
+select sucursal_id,dia_descanso,telefono_atencion from sucursal_taller_f4;
+
+--
+-- SERVICIO_LAPTOP
+--
+create or replace view servicio_laptop as
+select num_servicio,laptop_id,importe,diagnostico,factura,sucursal_id from servicio_laptop_f1
+union all
+select num_servicio,laptop_id,importe,diagnostico,factura,sucursal_id from servicio_laptop_f2
+union all
+select num_servicio,laptop_id,importe,diagnostico,factura,sucursal_id from servicio_laptop_f3
+union all
+select num_servicio,laptop_id,importe,diagnostico,factura,sucursal_id from servicio_laptop_f4;
+
+--
+-- LAPTOP_INVENTARIO
+--
+create or replace view laptop_inventario as
+select f1.laptop_id,f1.fecha_status,f2.rfc_cliente,f2.num_cuenta,f1.status_laptop_id,f1.sucursal_id
+from laptop_inventario_f1 f1, laptop_inventario_f2 f2
+where f1.laptop_id = f2.laptop_id;
+
+--
+-- H_STATUS_LAPTOP
+--
+create or replace view h_status_laptop as
+select h_status_laptop_id, laptop_id, fecha_status, status_laptop_id from h_status_laptop_f1
+union all
+select h_status_laptop_id, laptop_id, fecha_status, status_laptop_id from h_status_laptop_f2;
