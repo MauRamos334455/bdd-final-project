af70575d4a227abae431c1b9ca0159425f706ee4
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-01-07 16:55:42 -0600

    Error al terminar el comando [FIXED]

--- src/s-05-ilap-crv-s1-vistas-blob.sql
+++ src/s-05-ilap-crv-s1-vistas-blob.sql
@@ -44,6 +44,7 @@ from servicio_laptop_f2
 union all
 select num_servicio,laptop_id,importe,diagnostico,factura,sucursal_id
 from servicio_laptop_f3
+union all
 select num_servicio,laptop_id,importe,diagnostico,
   get_remote_serv_lap_f4_by_id(num_servicio,laptop_id) factura,
   sucursal_id
