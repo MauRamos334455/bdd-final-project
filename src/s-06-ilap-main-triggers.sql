359cd6ec3a29189dfa8a52243c008f02cecf9ddf
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-01-07 20:55:49 -0600

    Pruebas de triggers

--- src/s-06-ilap-main-triggers.sql
+++ src/s-06-ilap-main-triggers.sql
@@ -6,64 +6,109 @@
 clear screen
 whenever sqlerror exit rollback
 prompt ==============================
-prompt Creando triggers en ckvbdd_s1
+prompt CREANDO TRIGGERS EN CKVBDD_S1
 prompt ==============================
 connect ilap_bdd/ilap@ckvbdd_s1
 @s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-jrc-s1-sucursal-taller-trigger.sql
-@s-06-ilap-jrc-s1-sucursal-venta-trigger.sql
+show errors
+@s-06-ilap-ckv-s1-sucursal-taller-trigger.sql
+show errors
+@s-06-ilap-ckv-s1-sucursal-venta-trigger.sql
+show errors
 @s-06-ilap-laptop-trigger.sql
+show errors
 @s-06-ilap-laptop-inventario-trigger.sql
+show errors
 @s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-jrc-s1-servicio-laptop-trigger.sql
+show errors
+@s-06-ilap-ckv-s1-servicio-laptop-trigger.sql
+show errors
 @s-06-ilap-tipo-procesador-trigger.sql
+show errors
 @s-06-ilap-tipo-almacenamiento-trigger.sql
+show errors
 @s-06-ilap-tipo-monitor-trigger.sql
+show errors
 @s-06-ilap-tipo-tarjeta-video-trigger.sql
-prompt ==============================
-prompt Creando triggers en ckvbdd_s2
-prompt ==============================
-connect ilap_bdd/ilap@ckvbdd_s2
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-jrc-s2-sucursal-taller-trigger.sql
-@s-06-ilap-jrc-s2-sucursal-venta-trigger.sql
-@s-06-ilap-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-jrc-s2-servicio-laptop-trigger.sql
-@s-06-ilap-tipo-procesador-trigger.sql
-@s-06-ilap-tipo-almacenamiento-trigger.sql
-@s-06-ilap-tipo-monitor-trigger.sql
-@s-06-ilap-tipo-tarjeta-video-trigger.sql
-prompt ==============================
-prompt Creando triggers en crvbdd_s1
-prompt ==============================
-connect ilap_bdd/ilap@crvbdd_s1
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-arc-s1-sucursal-taller-trigger.sql
-@s-06-ilap-arc-s1-sucursal-venta-trigger.sql
-@s-06-ilap-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-arc-s1-servicio-laptop-trigger.sql
-@s-06-ilap-tipo-procesador-trigger.sql
-@s-06-ilap-tipo-almacenamiento-trigger.sql
-@s-06-ilap-tipo-monitor-trigger.sql
-@s-06-ilap-tipo-tarjeta-video-trigger.sql
-prompt ==============================
-prompt Creando triggers en crvbdd_s2
-prompt ==============================
-connect ilap_bdd/ilap@crvbdd_s2
-@s-06-ilap-sucursal-trigger.sql
-@s-06-ilap-arc-s2-sucursal-taller-trigger.sql
-@s-06-ilap-arc-s2-sucursal-venta-trigger.sql
-@s-06-ilap-arc-s2-laptop-trigger.sql
-@s-06-ilap-laptop-inventario-trigger.sql
-@s-06-ilap-historico-status-laptop-trigger.sql
-@s-06-ilap-arc-s2-servicio-laptop-trigger.sql
-@s-06-ilap-tipo-procesador-trigger.sql
-@s-06-ilap-tipo-almacenamiento-trigger.sql
-@s-06-ilap-tipo-monitor-trigger.sql
-@s-06-ilap-tipo-tarjeta-video-trigger.sql
-prompt Listo!
+show errors
+-- prompt ==============================
+-- prompt CREANDO TRIGGERS EN CKVBDD_S2
+-- prompt ==============================
+-- connect ilap_bdd/ilap@ckvbdd_s2
+-- @s-06-ilap-sucursal-trigger.sql
+-- show errors
+-- @s-06-ilap-ckv-s2-sucursal-taller-trigger.sql
+-- show errors
+-- @s-06-ilap-ckv-s2-sucursal-venta-trigger.sql
+-- show errors
+-- @s-06-ilap-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-laptop-inventario-trigger.sql
+-- show errors
+-- @s-06-ilap-laptop-inventario-trigger.sql
+-- show errors
+-- @s-06-ilap-historico-status-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-ckv-s2-servicio-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-procesador-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-almacenamiento-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-monitor-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-tarjeta-video-trigger.sql
+-- show errors
+-- prompt ==============================
+-- prompt CREANDO TRIGGERS EN CRVBDD_S1
+-- prompt ==============================
+-- connect ilap_bdd/ilap@crvbdd_s1
+-- @s-06-ilap-sucursal-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s1-sucursal-taller-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s1-sucursal-venta-trigger.sql
+-- show errors
+-- @s-06-ilap-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-laptop-inventario-trigger.sql
+-- show errors
+-- @s-06-ilap-historico-status-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s1-servicio-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-procesador-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-almacenamiento-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-monitor-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-tarjeta-video-trigger.sql
+-- show errors
+-- prompt ==============================
+-- prompt CREANDO TRIGGERS EN CRVBDD_S2
+-- prompt ==============================
+-- connect ilap_bdd/ilap@crvbdd_s2
+-- @s-06-ilap-sucursal-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s2-sucursal-taller-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s2-sucursal-venta-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s2-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-laptop-inventario-trigger.sql
+-- show errors
+-- @s-06-ilap-historico-status-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-crv-s2-servicio-laptop-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-procesador-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-almacenamiento-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-monitor-trigger.sql
+-- show errors
+-- @s-06-ilap-tipo-tarjeta-video-trigger.sql
+-- show errors
+-- prompt LISTO!
