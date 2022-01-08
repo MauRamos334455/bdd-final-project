19eb8e24864e77e8874738e3d178a90a7aefafee
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-01-08 02:18:18 -0600

    ERROR EN TRIGGER

--- src/s-06-ilap-crv-s1-sucursal-venta-trigger.sql
+++ src/s-06-ilap-crv-s1-sucursal-venta-trigger.sql
@@ -61,7 +61,7 @@ begin
       when deleting then 
         select count(*) into v_count
         from sucursal_f3
-        where sucursal_id = :new.sucursal_id;
+        where sucursal_id = :old.sucursal_id;
 
         if v_count > 0 then
           delete from sucursal_venta_f3 where sucursal_id = :old.sucursal_id;
@@ -69,7 +69,7 @@ begin
         else 
           select count(*) into v_count
           from sucursal_f2
-          where sucursal_id = :new.sucursal_id;
+          where sucursal_id = :old.sucursal_id;
           
           if v_count > 0 then
             delete from sucursal_venta_f2 where sucursal_id = :old.sucursal_id;
@@ -77,21 +77,21 @@ begin
           else
             select count(*) into v_count
             from sucursal_f1
-            where sucursal_id = :new.sucursal_id;
+            where sucursal_id = :old.sucursal_id;
           
             if v_count > 0 then
               delete from sucursal_venta_f1 where sucursal_id = :old.sucursal_id;
             else
               select count(*) into v_count
               from sucursal_f4
-              where sucursal_id = :new.sucursal_id;
+              where sucursal_id = :old.sucursal_id;
             
               if v_count > 0 then
                 delete from sucursal_venta_f4 where sucursal_id = :old.sucursal_id;
               else
                 raise_application_error(-20020,
                   'Error de integridad para el campo sucursal_id : '
-                  ||  :new.sucursal_id
+                  ||  :old.sucursal_id
                   || ' No se encontró el registro padre en ningún fragmento.');
               end if;
             end if;
