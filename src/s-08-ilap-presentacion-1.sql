7f51f3d784af1a6de3e74476dec4aa87c970bf00
Mauricio <47121742+MauRamos334455@users.noreply.github.com>
2022-01-07 21:20:48 -0600

    Triggers aparentemente listos

--- src/s-08-ilap-presentacion-1.sql
+++ src/s-08-ilap-presentacion-1.sql
@@ -12,5 +12,5 @@ prompt Iniciando con la creacion de la BDD.
 @s-03-ilap-main-ddl.sql
 @s-04-ilap-main-sinonimos.sql
 @s-05-ilap-main-vistas.sql
---@s-06-ilap-main-triggers.sql
+@s-06-ilap-main-triggers.sql
 --@s-07-ilap-main-soporte-blobs.sql
